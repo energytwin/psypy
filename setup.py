6785e7cca439c35763e6a8e3635c2367e0fa07af
jansiroky <56125822+jansiroky@users.noreply.github.com>
2025-08-22 09:09:29 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="psypy",
+    version="0.1.3-et1",
+    packages=find_packages(),
+    license="MIT",
+    description="Fork of psypy with EnergyTwin fixes",
+    author="EnergyTwin",
+)
